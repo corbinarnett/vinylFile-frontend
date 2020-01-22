22be087d261142d2d765634ab356b0d0fc66353f
corbinarnett <corbin.c.arnett@gmail.com>
2020-01-21 20:11:05 -0800

    change app to class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    App
-  );
+class App extends React.Component() {
+  render() {
+    return <div className="App">App</div>;
+  }
 }
 
 export default App;

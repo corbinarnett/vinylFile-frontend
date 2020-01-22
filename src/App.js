969feca4e8679177f5652a0e81639221ef9fb77b
corbinarnett <corbin.c.arnett@gmail.com>
2020-01-21 23:15:57 -0800

    strip down create-react-app file structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
-class App extends React.Component() {
+class App extends React.Component {
   render() {
-    return <div className="App">App</div>;
+    return ( 
+    <div className="App">
+      App
+    </div>
+    );
   }
 }
 

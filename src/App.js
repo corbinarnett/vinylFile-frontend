172069a06330ea233a82371e55b23268314bfa12
corbinarnett <corbin.c.arnett@gmail.com>
2020-01-22 15:53:12 -0800

    add componentDidMount to check client is fetching from rails api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
 
 class App extends React.Component {
+
+
+  // componentDidMount() {
+  //   fetch('http://localhost:3000/api/v1/albums')
+  //   .then(res => res.json())
+  //   .then(data => console.log(data))
+  // }
+
+
   render() {
     return ( 
     <div className="App">

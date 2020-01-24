e42b769cfe6702e7fa8782d59c3941e4df66e36f
corbinarnett <corbin.c.arnett@gmail.com>
2020-01-24 15:17:04 -0800

    import react-router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
+import {BrowserRouter as Router} from 'react-router-dom' 
 import albumReducer from './reducers/albumReducer'
 import App from "./App";
 
@@ -12,7 +13,9 @@ let store = createStore(albumReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById("root")
 );

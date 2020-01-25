b40bfb2d29fd74373ffbef07498bdbdbc3c24df5
corbinarnett <corbin.c.arnett@gmail.com>
2020-01-25 12:16:34 -0800

    change app component to now fetch albums from redux store

--- src/App.js
+++ src/App.js
@@ -1,3 +1,33 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch, } from 'react-router-dom';
+import { connect } from 'react-redux';
+import {fetchAlbums} from './actions/albumActions'
+
+import AlbumsContainer from './containers/AlbumsContainer'
+
+
+class App extends Component {
+
+componentDidMount() {
+  this.props.fetchAlbums();
+}
+
+  render () {
+    return (
+      <div>
+        <Router>
+          <Switch>
+            <Route exact path='/' component={AlbumsContainer}/>
+          </Switch>
+        </Router>
+      </div>
+    )
+  }
+}
+
+
+export default connect(null, { fetchAlbums })(App);
+
 // import React from "react";
 // // import { connect } from 'react-redux';
 // // import {fetchAlbums} from './actions/fetchAlbums'

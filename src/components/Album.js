d03054c11f307764e98a67b8f4b3e2f4fbfb7eb0
corbinarnett <corbin.c.arnett@gmail.com>
2020-01-26 01:20:34 -0800

    format Album component

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,23 +1,18 @@
-import React from 'react'
-import ReviewsContainer from '../containers/ReviewsContainer'
+import React from "react";
+import ReviewsContainer from "../containers/ReviewsContainer";
 
-
-
-const Album = (props) => {
-
-  let album = props.albums.filter(album => album.id == props.match.params.id)[0]
+const Album = props => {
+  let album = props.albums.filter(
+    album => album.id == props.match.params.id
+  )[0];
 
   return (
-
     <div>
-      <h2>
-        {album && album.name}
-      </h2>
-      <ReviewsContainer album={album}/>
+      <h2>{album && album.name}</h2>
+
+      <ReviewsContainer album={album} />
     </div>
-  )
+  );
+};
 
-
-}
-
-export default Album
+export default Album;

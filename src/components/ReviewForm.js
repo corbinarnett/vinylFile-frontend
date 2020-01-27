b10ae20879be76e5ea94fee6590d0463e0729a7e
corbinarnett <corbin.c.arnett@gmail.com>
2020-01-26 18:49:27 -0800

    add bootstrap styling to new review form

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { createReview } from "../actions/reviewActions";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
 
 class ReviewForm extends Component {
   state = {
@@ -28,50 +31,95 @@ class ReviewForm extends Component {
 
   render() {
     return (
-      <div>
-        <fieldset>
-          <h3>New Review Form</h3>
-          <form onSubmit={this.handleSubmit}>
-            <label>Username: </label>
-            <input
+      // <div>
+      //   <fieldset>
+      //     <h3>New Review Form</h3>
+      //     <form onSubmit={this.handleSubmit}>
+      //       <label>Username: </label>
+      //       <input
+      //         type="text"
+      //         placeholder="username"
+      //         name="username"
+      //         value={this.state.username}
+      //         onChange={this.handleChange}
+      //         required
+      //       />{" "}
+      //       <br />
+      //       <br />
+      //       <label>Content: </label>
+      //       <textarea
+      //         rows="4"
+      //         cols="50"
+      //         placeholder="Your Review"
+      //         name="content"
+      //         value={this.state.content}
+      //         onChange={this.handleChange}
+      //         required
+      //       />{" "}
+      //       <br />
+      //       <br />
+      //       <label>Rating: </label>
+      //       <input
+      //         min="1"
+      //         max="10"
+      //         type="number"
+      //         placeholder="1-10"
+      //         name="rating"
+      //         value={this.state.rating}
+      //         onChange={this.handleChange}
+      //         required
+      //       />{" "}
+      //       <br />
+      //       <br />
+      //       <input type="submit" />
+      //     </form>
+      //   </fieldset>
+      // </div>
+      <Container>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group controlId="formBasic">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
               type="text"
-              placeholder="username"
+              placeholder="Username"
               name="username"
               value={this.state.username}
               onChange={this.handleChange}
               required
-            />{" "}
-            <br />
-            <br />
-            <label>Content: </label>
-            <textarea
-              rows="4"
-              cols="50"
-              placeholder="Your Review"
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formBasic">
+            <Form.Label>Content</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Content"
               name="content"
               value={this.state.content}
               onChange={this.handleChange}
               required
-            />{" "}
-            <br />
-            <br />
-            <label>Rating: </label>
-            <input
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formBasic">
+            <Form.Label>Rating</Form.Label>
+            <Form.Control
+              type="text"
               min="1"
               max="10"
-              type="number"
-              placeholder="1-10"
+              placeholder="Rating"
               name="rating"
               value={this.state.rating}
               onChange={this.handleChange}
               required
-            />{" "}
-            <br />
-            <br />
-            <input type="submit" />
-          </form>
-        </fieldset>
-      </div>
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
     );
   }
 }

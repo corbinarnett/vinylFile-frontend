7f24c5e3f1ce8a060f2bd48cdf971b98cbd45c77
corbinarnett <corbin.c.arnett@gmail.com>
2020-02-04 15:06:23 -0800

    create store.js file to hold store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
-import rootReducer from "./reducers/rootReducer";
+import store from "./store";
 import App from "./App";
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
